99f7b7996948b9e99399b2441313add24cf235f5
Ben Garski <blgarski@uwm.edu>
2020-04-12 21:52:02 -0500

    Fixed radio default selector

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ ui <- miniPage(
                  miniContentPanel(
                     fillRow(
                       fillCol(
-                        radioButtons("difficulty", "Select Difficulty", choices = c("Beginner", "Intermediate", "Advanced"), selected = 1),
+                        radioButtons("difficulty", "Select Difficulty", choices = c("Beginner", "Intermediate", "Advanced"), selected = "Beginner"),
                         textInput("phone", "Phone Number", width = "80%"),
                         actionButton("textMe", "Send")
                         
