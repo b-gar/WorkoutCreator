1d9d250f973a5b2f20d6129816b0604f0d584e90
Ben Garski <blgarski@uwm.edu>
2020-04-12 20:54:29 -0500

    Changed tab names and structured server code

--- app.R
+++ app.R
@@ -1,27 +1,26 @@
 library(shiny)
 library(miniUI)
 library(twilio)
+library(gmailr)
 library(dplyr)
 
-keys <- read.csv("tokens.csv")
-Sys.setenv(TWILIO_SID = keys$sid[1])
-Sys.setenv(TWILIO_TOKEN = keys$token[1])
+use_secret_file("credentials.json")
 fromNumber <- "18646252951"
 
 ui <- miniPage(
   miniTitleBar("Workout Creator"),
   miniTabstripPanel(
-    miniTabPanel("Home", icon = icon("home"),
+    miniTabPanel("Bodyweight", icon = icon("walking"),
                  miniContentPanel(
                     fillRow(
-                      radioButtons("difficulty", "Select Difficulty", choices = c("Beginner", "Intermediate", "Advanced")),
+                      radioButtons("difficulty", "Select Difficulty", choices = c("Beginner", "Intermediate", "Advanced"), selected = 1),
                       sliderInput("time", "Select Exercise Duration", min = 5, max = 60, step = 5, value = 20)
                     )
                  )
     ),
     miniTabPanel("Kettlebell", icon = icon("weight-hanging"),
                  miniContentPanel(
-                   plotOutput("cars", height = "100%")
+                   
                  )
     ),
     miniTabPanel("Info", icon = icon("question-circle"),
@@ -36,9 +35,24 @@ server <- function(input, output, session) {
   
   # Get Workout in Correct Format
   
+  # Text It
+  observeEvent(input$textMe,{
+    keys <- read.csv("tokens.csv")
+    Sys.setenv(TWILIO_SID = keys$sid[1])
+    Sys.setenv(TWILIO_TOKEN = keys$token[1])
+    tw_send_message(from = fromNumber, to = input$phone, body = "")
+  })
   
-  #tw_send_message(from = fromNumber, to = input$phone, body = "")
-
+  # Email It
+  observeEvent(input$emailMe, {
+    email <- gm_mime(
+      To = input$email,
+      From = "",
+      Subject = "",
+      body = ""
+    )
+    gm_send_message(email)
+  })
 }
 
 shinyApp(ui, server)
