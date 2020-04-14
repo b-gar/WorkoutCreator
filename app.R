a4046e95b54496a4bde1e752f8c223290f95c323
Ben Garski <blgarski@uwm.edu>
2020-04-13 22:51:02 -0500

    ui adjustments

--- app.R
+++ app.R
@@ -18,17 +18,17 @@ ui <- miniPage(
                  )
     ),
     miniTabPanel("Kettlebell", icon = icon("weight-hanging"),
-                 miniContentPanel(
+                 miniContentPanel(padding = 0,
                    fillRow(
-                     radioButtons("kdifficulty", "Select Difficulty", choices = c("Beginner", "Intermediate", "Advanced"),
-                                  selected = "Beginner"),
-                     sliderInput("kduration", "Select Exercise Duration", min = 5, max = 60, step = 5, value = 20),
+                     fillCol(
+                       radioButtons("kdifficulty", "Select Difficulty", choices = c("Beginner", "Intermediate", "Advanced"),
+                                    selected = "Beginner"),
+                       sliderInput("kduration", "Select Exercise Duration", min = 5, max = 60, step = 5, value = 20),
+                       withSpinner(DTOutput("kbtable"), type = 7, color = "blue", size = 1)
+                     )
                    ),
                    miniButtonBlock(
                      actionButton("kcreate", "Create Workout", icon = icon("magic"), width = "100%")
-                   ),
-                   fillRow(
-                     withSpinner(DTOutput("kbtable"), type = 7, color = "blue", size = 1)
                    )
                  )
     ),
