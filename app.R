1170a479a4bcfd824473bd3ecf4e533f882318e0
Ben Garski <blgarski@uwm.edu>
2020-04-15 11:56:37 -0500

    added info page text

--- app.R
+++ app.R
@@ -37,7 +37,14 @@ ui <- miniPage(
     ),
     miniTabPanel("Info", icon = icon("question-circle"),
                  miniContentPanel(
-                    
+                    h2("The difficulty is based off the following:"),
+                    ul("Beginner: 20 second duration sets"),
+                    ul("Intermediate: 30 second duration sets"),
+                    ul("Advanced: 45 second duration sets"),
+                    h2("This will not be perfect! Please improvise if necessary. Kettlebell weight will be a huge factor in this as well."),
+                    br(),
+                    br(),
+                    h2("Please send any comments or requests to: shiny.workoutcreator@gmail.com")
                  )
     )
   )
